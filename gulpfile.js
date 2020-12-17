5bcb06f71473538574704c92560370efc5b78fe6
Amalie <amal3983@edu.easj.dk>
2020-12-17 09:17:52 +0100

    Ændret package.json og gulpfile.js til Kims nye version, så CSS bliver minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
-const gulp = require ("gulp");
-const sass = require ("gulp-sass");
-const sourcemaps = require ("gulp-sourcemaps");
-const postcss = require ("gulp-postcss");
-const autoprefixer = require ("autoprefixer");
-const cssnano = require ("cssnano");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 const terser = require("gulp-terser");
 const rename = require("gulp-rename");
-
 
 function styles() {
 
@@ -14,13 +13,12 @@ function styles() {
         gulp.src("css/*.scss")
             .pipe(sourcemaps.init())
             .pipe(sass())
-            // .pipe(postcss([autoprefixer({grid:true}), cssnano()]))
+            .pipe(postcss([autoprefixer({grid:true}), cssnano()]))
             .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
     );
 
 }
-
 
 function js() {
     return(
@@ -33,14 +31,12 @@ function js() {
     );
 }
 
-
 function watch(){
     gulp.watch("css/*.scss", styles);
-    gulp.watch(["js/*.js", "!js/*min.js"], js)
+    gulp.watch(["js/*.js", "!js/*min.js"], js);
 }
 
 const build = gulp.parallel(styles, js);
-
 
 exports.styles = styles;
 exports.js = js;
